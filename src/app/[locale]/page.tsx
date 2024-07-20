7057ee5bccd3eafacbeb7cae0fc11e9aa6a2c1b0
useEffects <joel.sr1024@gmail.com>
2024-07-20 18:59:18 +0530

    changes

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -9,30 +9,36 @@ import { Carousel } from '@/components/carousel';
 import { ReactNode } from 'react';
 import MissionImg from "@/assets/mission.png";
 import ShareDrinkImg from "@/assets/share-drink.jpg";
+import ChangeLocale from '@/components/change-locale';
+import InstagramImg from "@/assets/instagram-1-svgrepo-com.svg"
 
 export default async function Home({ params: { locale } }: { params: { locale: LocaleTypes } }) {
   const { t } = await createTranslation(locale, "home")
   // @ts-ignore
   return <Fullpage>
-    <FullPageSections>
-      <FullpageSection className="bg-dark">
-        <div className='flex flex-col-reverse md:flex-row justify-center items-center xl:container p-0 w-full h-full bg-dark-50'>
-          <div className='w-full h-1/2 md:w-1/2 md:h-full flex flex-col gap-4 bg-gradient-to-t md:bg-gradient-to-r from-dark/80 to-dark p-4'>
-            <div className='flex-grow flex flex-col justify-evenly gap-4'>
+    <FullPageSections className='bg-gradient-to-t md:bg-gradient-to-r from-dark-50 via-dark to-dark'>
+      <FullpageSection className='p-4'>
+        <div className='flex flex-col md:flex-row justify-center items-center container mx-auto gap-4 w-full h-full'>
+          <div className='w-full h-1/2 md:w-1/2 md:h-full flex flex-col gap-4'>
+            <div className='flex-grow flex flex-col justify-center gap-4 md:gap-12 items-center w-full'>
+              <div className='flex gap-4 w-full justify-between'>
+                <img src={logoImg.src} alt="" className='md:w-1/2 object-contain' />
+                <ChangeLocale />
+              </div>
               <div>
                 <p className='heading text-sky tracking-widest'>POWERBOOM</p>
                 <p className='heading text-sky tracking-widest opacity-50'>POWERBOOM</p>
-                <p className='heading text-sky tracking-widest opacity-5'>POWERBOOM</p>
+                <p className='heading text-sky tracking-widest opacity-10'>POWERBOOM</p>
               </div>
-              <p className='text-lg'>{t("about-line")}</p>
-              <div className='flex flex-col md:flex-row gap-4 items-center justify-center'>
-                <img src={logoImg.src} alt="" className='md:w-1/2 object-contain' />
-                <div className='flex items-end flex-grow h-full justify-center'>
-                  <button className='bg-white rounded-full px-4 py-2 flex gap-[1ch] hover:opacity-80 cursor-pointer items-center'>
-                    <span className='text-dark uppercase'>{t("explore-the-taste")}</span>
-                    <img src={ArrowRightUp.src} alt="" className='h-8 object-contain' />
-                  </button>
-                </div>
+              <div className='flex flex-col md:flex-row gap-4 items-center justify-center w-full'>
+                <button className='bg-white w-full rounded-full px-4 py-2 flex gap-[1ch] hover:opacity-80 active:opacity-80 cursor-pointer items-center justify-center'>
+                  <span className='text-dark uppercase'>{t("explore-the-taste")}</span>
+                  <img src={ArrowRightUp.src} alt="" className='h-8 object-contain' />
+                </button>
+                <button className='bg-white w-full rounded-full px-4 py-2 flex gap-[1ch] hover:opacity-80 active:opacity-80 cursor-pointer items-center justify-center'>
+                  <span className='text-dark uppercase'>{t("download-company-profile")}</span>
+                  <img src={ArrowRightUp.src} alt="" className='h-8 object-contain' />
+                </button>
               </div>
             </div>
           </div>
@@ -42,17 +48,22 @@ export default async function Home({ params: { locale } }: { params: { locale: L
           </div>
         </div>
       </FullpageSection>
-      <FullpageSection>
-        <video className='w-full h-full object-cover bg-transparent' src="/assets/powerboomvideo.mp4" autoPlay={true} muted loop></video>
+      <FullpageSection className='relative'>
+        <video className='w-full h-full object-cover bg-transparent absolute top-0 bottom-0 left-0 right-0' src="/assets/powerboomvideo.mp4" autoPlay={true} muted loop />
+        <div className='absolute top-0 bottom-0 left-0 right-0 bg-black/80' />
+        <div className='flex flex-col gap-4 p-4 justify-end w-full h-full relative z-10'>
+          <p className='heading !text-3xl text-sky/50'>{t("do-not-miss")}</p>
+          <img src={InstagramImg.src} className='w-20 h-20' alt="" />
+        </div>
       </FullpageSection>
-      <FullpageSection>
-        <Carousel showThumbs={false} className='w-full h-full' showStatus={false}>
-          <CarouselItem cover={MissionImg.src} heading={t("Mission")}>
-            <p className='text-xl max-w-sm'>
+      <FullpageSection className='bg-dark'>
+        <Carousel showThumbs={false} className='h-full container mx-auto p-0' showStatus={false} swipeable >
+          <CarouselItem cover={ShareDrinkImg.src} heading={t("Mission")}>
+            <p className='text-3xl max-w-sm text-left'>
               {t("mission")}
             </p>
           </CarouselItem>
-          <CarouselItem cover={ShareDrinkImg.src} heading={t("Mission")}>
+          <CarouselItem cover={MissionImg.src} heading={t("Mission")}>
             <p className='text-xl max-w-sm'>
               {t("mission")}
             </p>
@@ -69,17 +80,20 @@ export default async function Home({ params: { locale } }: { params: { locale: L
           </CarouselItem>
         </Carousel>
       </FullpageSection>
+      <FullpageSection>
+
+      </FullpageSection>
     </FullPageSections>
-  </Fullpage>
+  </Fullpage >
 }
 
 const CarouselItem = ({ children, cover, heading }: { children: ReactNode, cover: string, heading: string }) => {
-  return <div className='flex flex-col md:flex-row gap-4 h-screen bg-gradient-to-br from-dark-50 via-dark to-dark'>
+  return <div className='flex flex-col md:flex-row gap-4 h-screen'>
     <div className='w-full h-1/2 md:w-1/2 md:h-full relative'>
       <p className='absolute heading top-4 left-4'>{heading}</p>
-      <img src={cover} alt="" className='w-full h-full object-cover' />
+      <img src={cover} alt="" className='w-full h-full object-contain p-12' />
     </div>
-    <div className='w-full h-1/2 md:w-1/2 md:h-full flex justify-center items-center'>
+    <div className='w-full h-1/2 md:w-1/2 md:h-full flex justify-center'>
       {children}
     </div>
   </div>
--- src/components/change-locale.tsx
+++ src/components/change-locale.tsx
@@ -2,27 +2,43 @@
 
 import React from 'react';
 import { useRouter, useParams, useSelectedLayoutSegments } from 'next/navigation';
+import FrenchFlagImg from "@/assets/france-svgrepo-com.svg"
+import EnglishFlagImg from "@/assets/united-kingdom-svgrepo-com.svg"
+import UAEFlagImg from "@/assets/united-arab-emirates-svgrepo-com.svg"
+import Link from 'next/link';
+import { LocaleTypes } from '@/lib/contants';
 
 const ChangeLocale = () => {
     const router = useRouter();
     const params = useParams();
     const urlSegments = useSelectedLayoutSegments();
 
-    const handleLocaleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const newLocale = e.target.value;
-
-        router.push(`/${newLocale}/${urlSegments.join('/')}`);
+    const handleLocaleChange = (newLocale: string) => {
+        router.replace(`/${newLocale}/${urlSegments.join('/')}`);
     };
 
     return (
-        <div>
-            <select onChange={e => handleLocaleChange(e)} value={params.locale}>
-                <option value="en">🇺🇸 English</option>
-                <option value="fr">🇨🇳 中文</option>
-                <option value="ar">🇸🇪 Swedish</option>
-            </select>
+        <div className='flex flex-col md:flex-row gap-2'>
+            {options.map(({ value, flag }) => <button onClick={() => handleLocaleChange(value)} key={value} className='flex gap-2 items-center justify-center hover:opacity-80 active:opacity-80'>
+                <img src={flag} alt="" className='h-8 w-8 rounded-full' />
+            </button>)}
         </div>
     );
 };
+
+const options = [
+    {
+        value: "en",
+        flag: EnglishFlagImg.src
+    },
+    {
+        value: "fr-FR",
+        flag: FrenchFlagImg.src
+    },
+    {
+        value: "ar-AE",
+        flag: UAEFlagImg.src
+    }
+] as { value: LocaleTypes, flag: string }[]
 
 export default ChangeLocale;
